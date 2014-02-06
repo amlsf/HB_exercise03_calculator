8fd68bf4deb87ed2abed841328e4b92b16e5a38e
Hackbright Student <info@hackbrightacademy.com>
2014-02-05 16:39:49 -0800

    Added validation for various user inputs

--- calculator.py
+++ calculator.py
@@ -7,23 +7,32 @@ useraction = raw_input("Enter command followed by operators, separated by space 
 
 calc_list = useraction.split(" ")
 
-if calc_list[0] == "+":
-    print arithmetic.add(int(calc_list[1]), int(calc_list[2]))
-elif calc_list[0] == "-":
-    print arithmetic.subtract(int(calc_list[1]), int(calc_list[2]))
-elif calc_list[0] == "*":
-    print arithmetic.multiply(int(calc_list[1]), int(calc_list[2]))
-elif calc_list[0] == "/":
-    print arithmetic.divide(int(calc_list[1]), int(calc_list[2]))
-elif calc_list[0] == "square":
-    print arithmetic.square(int(calc_list[1]))
-elif calc_list[0] == "cube":
-    print arithmetic.cube(int(calc_list[1]))
-elif calc_list[0] == "power":
-    print arithmetic.power(int(calc_list[1]), int(calc_list[2]))    
-elif calc_list[0] == "mod":
-    print arithmetic.mod(int(calc_list[1]), int(calc_list[2]))
-elif calc_list[0] == "q":
+if calc_list[0] == "q":
     print "Exiting the calculator"
+
+elif len(calc_list) == 2 and calc_list[1].isdigit() == True:
+    if calc_list[0] == "square":
+        print arithmetic.square(int(calc_list[1]))
+    elif calc_list[0] == "cube":
+        print arithmetic.cube(int(calc_list[1]))
+    else: 
+        print "Invalid input"
+
+elif len(calc_list) == 3 and calc_list[1].isdigit() == True and calc_list[2].isdigit() == True:
+    if calc_list[0] == "+":
+        print arithmetic.add(int(calc_list[1]), int(calc_list[2]))
+    elif calc_list[0] == "-":
+        print arithmetic.subtract(int(calc_list[1]), int(calc_list[2]))
+    elif calc_list[0] == "*":
+        print arithmetic.multiply(int(calc_list[1]), int(calc_list[2]))
+    elif calc_list[0] == "/":
+        print arithmetic.divide(int(calc_list[1]), int(calc_list[2]))
+    elif calc_list[0] == "power":
+        print arithmetic.power(int(calc_list[1]), int(calc_list[2]))    
+    elif calc_list[0] == "mod":
+        print arithmetic.mod(int(calc_list[1]), int(calc_list[2]))
+    else: 
+        print "Invalid input"
+
 else: 
-    print "Incorrect input"
+    print "Invalid input"
